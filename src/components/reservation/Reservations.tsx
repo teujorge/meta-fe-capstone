559cdd50b80813dbcc4e0e668d24ffa088c989b4
Matheus Jorge <mrljorge@outlook.com>
2024-11-16 05:00:45 -0300

    feat: enhance reservation page with background image and styling

--- src/components/reservation/Reservations.tsx
+++ src/components/reservation/Reservations.tsx
@@ -3,7 +3,13 @@ import { BookingForm } from "./BookingForm";
 export function Reservations() {
   return (
     <main className="flex flex-col gap-28 limit-width p-8">
-      <h1>Book a table</h1>
+      <h1 className="text-primary pb-20">Book a table</h1>
+      <img
+        src="/assets/about/locale.png"
+        alt="restaurant interior"
+        className="absolute inset-0 w-full h-72 object-cover -z-10 brightness-50"
+      />
+
       <BookingForm />
     </main>
   );
